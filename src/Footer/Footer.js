3dc2334abb72d31a7f9f1383e58af9e1a6f92833
navjot369 <personal.navjotsingh@gmail.com>
2023-12-06 11:11:21 +0530

    Add footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,9 @@
 import './Footer.css';
 
 export default function Footer() {
+    return(
     <div className="footer-cont">
         <p className="inner-content-footer">FOOTER</p>
     </div>
+    );
 }
